cfb26e7d1dd97249cfe8656890a17f0884798c86
Sachin Koshti <sachin.koshti@gatewaytechnolabs.com>
2016-10-27 17:01:48 +0530

    Updated code

---
+++ js/Sample.js
@@ -0,0 +1,13 @@
+﻿var app = angular.module("myApp", []);
+
+app.controller("SampleCtrl", function ($scope) {
+    $scope.model = { firstName: "Gabriel", lastName: "Schenker" };
+
+    $scope.clickMe = function (name) {
+        alert("Hello " + name);
+    };
+
+    $scope.regex = '\\d{1,4}';
+});
+
+
